f713a788fd791b6e228daa1c76a08ee221277799
sudipeb <sudip.koirala@ebpearls.com>
2025-11-05 12:03:48 +0545

    debug print statment added

--- lib/features/counter/cubit/tap_cubit.dart
+++ lib/features/counter/cubit/tap_cubit.dart
@@ -1,8 +1,12 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 
 ///A [Cubit] which manages [int] as its state
 class TapCubit extends Cubit<int> {
   TapCubit() : super(0);
   //for each new taps on the screen increments the value
-  void onTap() => emit(state + 1);
+  void onTap() {
+    emit(state + 1);
+    debugPrint('Tapped! Current value: ${state + 1}');
+  }
 }
