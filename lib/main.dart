b259b501cb0f62bfb04a12028cb6178b98175f94
sudipeb <sudip.koirala@ebpearls.com>
2025-11-05 17:27:12 +0545

    hive initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:tapcounter/app_route.dart';
 import 'package:tapcounter/core/app_theme.dart';
 import 'package:tapcounter/features/counter/cubit/nav_bar_cubit.dart';
 import 'package:tapcounter/features/counter/cubit/tap_cubit.dart';
 import 'package:tapcounter/features/counter/cubit/timer_cubit.dart';
+import 'package:tapcounter/features/counter/models/tap_session_model.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Hive.initFlutter();
+
+  // Register the manual TypeAdapter
+  Hive.registerAdapter(TapSessionAdapter());
+
+  // Open the box
+  Hive.openBox<TapSession>('tap_sessions');
   runApp(const MyApp());
 }
 
