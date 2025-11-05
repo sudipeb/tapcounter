8af09b835f3c7d024061d008fc0bdbef24c48ee1
sudipeb <sudip.koirala@ebpearls.com>
2025-11-05 14:49:35 +0545

    cubit timer service created

---
+++ lib/features/counter/cubit/timer_cubit.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+
+class TimerCubit extends Cubit<int> {
+  Timer? _timer;
+  TimerCubit() : super(30);
+  void startTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (state > 0) {
+        emit(state - 1);
+      } else {
+        timer.cancel;
+      }
+    });
+  }
+
+  void stopTimer() {
+    _timer?.cancel();
+  }
+
+  @override
+  Future<void> close() {
+    _timer?.cancel();
+    return super.close();
+  }
+}
