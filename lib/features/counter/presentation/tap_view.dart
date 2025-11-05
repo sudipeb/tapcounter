5eea05e6b376c88175a6c98fbd6dcc47573584c9
sudipeb <sudip.koirala@ebpearls.com>
2025-11-05 11:43:53 +0545

    tapview finalized with cubit

--- lib/features/counter/presentation/tap_view.dart
+++ lib/features/counter/presentation/tap_view.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tapcounter/features/counter/cubit/tap_cubit.dart';
 
 class TapView extends StatelessWidget {
   const TapView({super.key});
   @override
   Widget build(context) {
-    return Scaffold(appBar: AppBar(title: Text('Faster Finger')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Faster Finger')),
+      body: GestureDetector(
+        onTap: () => context.read<TapCubit>().onTap,
+        child: SizedBox.expand(
+          child: Column(
+            children: [
+              BlocBuilder<TapCubit, int>(
+                builder: (context, state) => Text(
+                  '$state',
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+              ),
+              Text(
+                'Keep tapping as You can',
+                style: Theme.of(context).textTheme.displayMedium,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
