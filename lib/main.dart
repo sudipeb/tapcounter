a6c6d3f9b0191dda32fd44955ec2844a5373b971
sudipeb <sudip.koirala@ebpearls.com>
2025-11-05 14:05:59 +0545

    decalred the cubits using the multibloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tapcounter/core/app_theme.dart';
+import 'package:tapcounter/features/counter/cubit/tap_cubit.dart';
 import 'package:tapcounter/features/counter/presentation/tap_page.dart';
+import 'package:tapcounter/features/counter/presentation/tap_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +14,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const TapPage(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => TapCubit()),
+        BlocProvider(create: (_) => ToggleCubit()),
+      ],
+      child: BlocBuilder<ToggleCubit, ThemeMode>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: lightTheme,
+            darkTheme: darkTheme,
+            themeMode: ThemeMode.system,
+            home: const TapView(),
+          );
+        },
+      ),
     );
   }
 }
