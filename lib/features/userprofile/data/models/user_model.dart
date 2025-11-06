8d1be3acf587087702fbd9eecc0bb27075d58905
sudipeb <sudip.koirala@ebpearls.com>
2025-11-06 15:07:01 +0545

    user model created

---
+++ lib/features/userprofile/data/models/user_model.dart
@@ -0,0 +1,14 @@
+class User {
+  final int id;
+  final String name;
+  final String imageurl;
+  final int age;
+  final String email;
+  const User({
+    required this.id,
+    required this.name,
+    required this.imageurl,
+    required this.age,
+    required this.email,
+  });
+}
