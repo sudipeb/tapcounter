58c89aa8f7e04d5cfd50ac86f11cf55c82750270
sudipeb <sudip.koirala@ebpearls.com>
2025-11-06 11:11:14 +0545

    created the path for history and settings

--- lib/app_route.dart
+++ lib/app_route.dart
@@ -1,10 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:tapcounter/app_route.gr.dart';
 
-@AutoRouterConfig()
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
     AutoRoute(page: TapViewRoute.page, initial: true),
+    AutoRoute(page: HistoryRoute.page),
+    AutoRoute(page: SettingsRoute.page),
   ];
 }
