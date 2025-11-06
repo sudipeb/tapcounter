08815e566dc11c0b73f4557422e3e883211174c5
sudipeb <sudip.koirala@ebpearls.com>
2025-11-06 15:14:37 +0545

    created the typeadapter for the user

--- lib/features/userprofile/data/models/user_model.dart
+++ lib/features/userprofile/data/models/user_model.dart
@@ -1,14 +1,40 @@
+import 'package:hive/hive.dart';
+
 class User {
   final int id;
   final String name;
-  final String imageurl;
+  final String imageUrl;
   final int age;
   final String email;
   const User({
     required this.id,
     required this.name,
-    required this.imageurl,
+    required this.imageUrl,
     required this.age,
     required this.email,
   });
+}
+
+class UserAdapter extends TypeAdapter<User> {
+  @override
+  final int typeId = 0;
+  @override
+  User read(BinaryReader reader) {
+    final id = reader.readInt();
+    final name = reader.readString();
+    final imageUrl = reader.readString();
+    final email = reader.readString();
+    final age = reader.readInt();
+
+    return User(id: id, name: name, imageUrl: imageUrl, email: email, age: age);
+  }
+
+  @override
+  void write(BinaryWriter writer, User obj) {
+    writer.writeInt(obj.id);
+    writer.writeString(obj.name);
+    writer.writeString(obj.imageUrl);
+    writer.writeString(obj.email);
+    writer.writeInt(obj.age);
+  }
 }
