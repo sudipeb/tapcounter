9866a78c9059725ee682064220edc22ccf028811
sudipeb <sudip.koirala@ebpearls.com>
2025-11-13 11:39:48 +0545

    path setup for splash screen

--- lib/app_route.dart
+++ lib/app_route.dart
@@ -1,12 +1,13 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:tapcounter/app_route.gr.dart';
 
-@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+@AutoRouterConfig()
 class AppRouter extends RootStackRouter {
   @override
   List<AutoRoute> get routes => [
+    AutoRoute(page: SplashRoute.page, initial: true),
     AutoRoute(page: UserInputRoute.page),
-    AutoRoute(page: TapViewRoute.page, initial: true),
+    AutoRoute(page: TapViewRoute.page),
     AutoRoute(page: HistoryRoute.page),
     AutoRoute(page: SettingsRoute.page),
   ];
--- lib/app_route.gr.dart
+++ lib/app_route.gr.dart
@@ -9,24 +9,25 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i5;
-import 'package:flutter/material.dart' as _i6;
+import 'package:auto_route/auto_route.dart' as _i6;
+import 'package:flutter/material.dart' as _i7;
 import 'package:tapcounter/features/counter/presentation/history_page.dart'
     as _i1;
-import 'package:tapcounter/features/counter/presentation/tap_view.dart' as _i3;
+import 'package:tapcounter/features/counter/presentation/tap_view.dart' as _i4;
 import 'package:tapcounter/features/settings/user_settings.dart' as _i2;
 import 'package:tapcounter/features/userprofile/data/presentation/screens/user_input.dart'
-    as _i4;
+    as _i5;
+import 'package:tapcounter/features/splash/presentation/splash.dart' as _i3;
 
 /// generated route for
 /// [_i1.HistoryPage]
-class HistoryRoute extends _i5.PageRouteInfo<void> {
-  const HistoryRoute({List<_i5.PageRouteInfo>? children})
+class HistoryRoute extends _i6.PageRouteInfo<void> {
+  const HistoryRoute({List<_i6.PageRouteInfo>? children})
     : super(HistoryRoute.name, initialChildren: children);
 
   static const String name = 'HistoryRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i6.PageInfo page = _i6.PageInfo(
     name,
     builder: (data) {
       return const _i1.HistoryPage();
@@ -36,13 +37,13 @@ class HistoryRoute extends _i5.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.SettingsPage]
-class SettingsRoute extends _i5.PageRouteInfo<void> {
-  const SettingsRoute({List<_i5.PageRouteInfo>? children})
+class SettingsRoute extends _i6.PageRouteInfo<void> {
+  const SettingsRoute({List<_i6.PageRouteInfo>? children})
     : super(SettingsRoute.name, initialChildren: children);
 
   static const String name = 'SettingsRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i6.PageInfo page = _i6.PageInfo(
     name,
     builder: (data) {
       return const _i2.SettingsPage();
@@ -51,25 +52,41 @@ class SettingsRoute extends _i5.PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [_i3.TapViewPage]
-class TapViewRoute extends _i5.PageRouteInfo<void> {
-  const TapViewRoute({List<_i5.PageRouteInfo>? children})
-    : super(TapViewRoute.name, initialChildren: children);
+/// [_i3.SplashPage]
+class SplashRoute extends _i6.PageRouteInfo<void> {
+  const SplashRoute({List<_i6.PageRouteInfo>? children})
+    : super(SplashRoute.name, initialChildren: children);
 
-  static const String name = 'TapViewRoute';
+  static const String name = 'SplashRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i6.PageInfo page = _i6.PageInfo(
     name,
     builder: (data) {
-      return const _i3.TapViewPage();
+      return const _i3.SplashPage();
     },
   );
 }
 
 /// generated route for
-/// [_i4.UserInputPage]
-class UserInputRoute extends _i5.PageRouteInfo<UserInputRouteArgs> {
-  UserInputRoute({_i6.Key? key, List<_i5.PageRouteInfo>? children})
+/// [_i4.TapViewPage]
+class TapViewRoute extends _i6.PageRouteInfo<void> {
+  const TapViewRoute({List<_i6.PageRouteInfo>? children})
+    : super(TapViewRoute.name, initialChildren: children);
+
+  static const String name = 'TapViewRoute';
+
+  static _i6.PageInfo page = _i6.PageInfo(
+    name,
+    builder: (data) {
+      return const _i4.TapViewPage();
+    },
+  );
+}
+
+/// generated route for
+/// [_i5.UserInputPage]
+class UserInputRoute extends _i6.PageRouteInfo<UserInputRouteArgs> {
+  UserInputRoute({_i7.Key? key, List<_i6.PageRouteInfo>? children})
     : super(
         UserInputRoute.name,
         args: UserInputRouteArgs(key: key),
@@ -78,13 +95,13 @@ class UserInputRoute extends _i5.PageRouteInfo<UserInputRouteArgs> {
 
   static const String name = 'UserInputRoute';
 
-  static _i5.PageInfo page = _i5.PageInfo(
+  static _i6.PageInfo page = _i6.PageInfo(
     name,
     builder: (data) {
       final args = data.argsAs<UserInputRouteArgs>(
         orElse: () => const UserInputRouteArgs(),
       );
-      return _i4.UserInputPage(key: args.key);
+      return _i5.UserInputPage(key: args.key);
     },
   );
 }
@@ -92,7 +109,7 @@ class UserInputRoute extends _i5.PageRouteInfo<UserInputRouteArgs> {
 class UserInputRouteArgs {
   const UserInputRouteArgs({this.key});
 
-  final _i6.Key? key;
+  final _i7.Key? key;
 
   @override
   String toString() {
